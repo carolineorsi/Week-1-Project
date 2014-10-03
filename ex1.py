3df8eef7c4deccf0230b0ee1a76b0ed8082cd7e5
Hackbright Student <info@hackbrightacademy.com>
2014-10-02 17:14:51 -0700

    Updated with docstring.

--- ex1.py
+++ ex1.py
@@ -1,3 +1,12 @@
+"""Included in the exercise is a zipfile, 'files.zip'. It contains 200 files with random character strings for names, all lowercase. First, unzip this file into a directory named 'original_files'.
+
+Your job is to write a program, ex1.py, that does the following things:
+
+Create 26 directories in the current directory, one for each letter of the alphabet: ./a/ ./b/ ./c/ etc.
+
+Loop through all the files in the original_files directory, and organize each of those files into the directory that their name starts with.
+"""
+
 import os
 import string
 import shutil
